efd5b70cb5a318b67af7eac69884c292871e889e
Ashwin Bijoy <ashwinbijoywork@gmail.com>
2025-06-24 12:49:47 +0530

    Update easyapplyai.py

--- easyapplyai.py
+++ easyapplyai.py
@@ -1,1 +1,22 @@
-import 
+import streamlit as st
+import openai
+openai.apikey = "your_key_here"
+
+st.set_page_config(page_title="EasyApplyAi", layout="centered")
+
+st.title("EasyApplyAi")
+st.subheader("Tailor Your Resume and Cover Letter to Any Job — Instantly!")
+
+resume_input = st.text_area (
+    "Paste your Resume here",
+   placeholder="Paste your resume here...",
+    height=250
+)
+
+job_input = st.text_area (
+    "Paste your Job Description here",
+    placeholder="Paste your JD here...",
+    height=250    
+)
+
+generate_button = st.button ("Generate Custom Resume & Cover Letter")
